222873ec862f7b3cfcc0f718328334725605b450
AlfreThiago <hiranarizaga@gmail.com>
2025-06-02 23:37:09 -0300

    Lo mismo q antes

--- a.php
+++ a.php
@@ -1,10 +1,8 @@
 <?php
 session_start();
 
-// Verifica si la variable de sesión que indica que el usuario está logueado existe.
-// Ajusta 'loggedin' al nombre de la variable que usas para indicar el login.
+
 if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    // Si no está logueado, redirige a la página de login.
     header("Location: Cliente.php");
     exit;
 } else {
