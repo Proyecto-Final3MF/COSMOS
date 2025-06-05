38df6f4a2a75f1eab588cb8520cffb598146d283
AlfreThiago <hiranarizaga@gmail.com>
2025-06-04 22:03:01 -0300

    Tecnico Login y Registro
    Auto explicativo

--- Cliente/a.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
-    header("Location: Cliente.php");
-    exit;
-} else {
-    echo "Estas logeado";
-}
-?><a href="logout.php">Cerrar Session
