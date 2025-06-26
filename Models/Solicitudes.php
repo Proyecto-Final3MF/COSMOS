7c354cf6d6c7ae37e71ff0fbd06889b8a13a0405
FedeMosegui <agustinmosegui1200@gmail.com>
2025-06-25 21:18:26 -0300

    Actualización 25/6 21:18

---
+++ Models/Solicitudes.php
@@ -0,0 +1,32 @@
+<?php
+require_once("config/conexion.php");
+
+class Solicitud {
+    private $db;
+
+    public function __construct() {
+        $this->db = conectar();
+    }
+
+    public function guardar($usuario_id, $imagen, $categoria_id, $obs) {
+        $sql = "INSERT INTO solicitud (usuario_id, imagen, categoria_id, obs) VALUES ('$usuario_id, $imagen, $categoria_id, $obs')";
+        return $this->db->query($sql);
+    }
+
+    public function buscarPorId($id){
+        $sql = "SELECT * FROM solicitud WHERE id = $id";
+        return $this->db->query($sql)->fetch_assoc();
+    }
+
+    public function actualizar($id, $imagen, $categoria_id, $obs) {
+        $sql = "UPDATE solicitud SET imagen='$imagen' categoria_id='$categoria_id' obs='$obs', WHERE id='$id'";
+        return $this->db->query($sql);
+    }
+
+    public function borrar($id) {
+        $sql = "DELETE FROM solicitud WHERE id=$id";
+        return $this->db->query($sql);
+    }
+}
+
+?>
