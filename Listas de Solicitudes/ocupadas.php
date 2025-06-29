857b7197bb7c77ccf797e7f2d7b1c3b062146d8e
AlfreThiago <hiranarizaga@gmail.com>
2025-06-29 15:22:57 -0300

    Update ocupadas.php

--- Listas de Solicitudes/ocupadas.php
+++ Listas de Solicitudes/ocupadas.php
@@ -1,11 +1,28 @@
-    $sql = "SELECT solicitud.obs AS obs, estado.nombre AS estado FROM solicitud JOIN estado ON solicitud.estado_id = estado.id WHERE solicitud.estado_id != 1;"; 
+<?php
+
+require_once("../config/conexion.php");
+
+$conexion = conectar();
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Solicitus disponibles</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <?php
+    $sql = "SELECT solicitud.obs AS obs, estado.nombre AS estado FROM solicitud JOIN estado ON solicitud.estado_id = estado.id WHERE solicitud.estado_id != 1;";
 
     $resultado = $conexion->query($sql);
     if (!$resultado) {
         die("Error en la consulta: " . $conexion->error);
     }
     ?>
-    <h1>Listado de Solicitudes Aceptadas</h1>
+    <h1>Listado de Solicitudes disponibles</h1>
     <div id="solicitud-container">
         <form action="Selec.php">
         <?php
@@ -14,7 +31,7 @@
             <div class="solicitud">
                 <?php echo htmlspecialchars($fila['obs']); ?>
                 <?php echo htmlspecialchars($fila['estado']); ?>
-
+               
             </div>
         <?php
         }
