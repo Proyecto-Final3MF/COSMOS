ea67f62141dbe85b57fa3e30fe8e750cfc1dfe15
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-07-21 23:49:17 -0300

    cambios al usuario
    no nada

--- Index.php
+++ Index.php
@@ -7,11 +7,38 @@
     <link rel="stylesheet" href="Assets/css/index.css">
 </head>
 
-<?php
-    require_once("views/includes/header.php");
-?>
-
-<body>  
+<body>
+    <nav class="navbar">
+       
+        <a href="#" class="logo">
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQz59MAQOT2A_bbCS4Agn-5_40puEUcq69CM19b6obLlg5ugTM34Vs1wq65mAtsoCsv_Kc&usqp=CAU" height="55px" alt="logo con una T y A por tencicos y asociados :V"> 
+        </a>
+        
+        
+       
+        <ul class="nav-links" id="nav-links">
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Tecnicos</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="#">Contacto</a></li>
+            
+        </ul>
+        
+        
+        <div class="search-bar">
+            <input type="text" placeholder="Buscador para buscar" aria-label="Buscar">
+            <button type="submit">Buscar</button>
+        </div>
+        
+       
+        <div class="action-buttons">
+            <button class="btn btn-login">Iniciar sesión</button>
+            <button class="btn btn-signup pulse">Registrarse</button>
+        </div>
+    </nav>
+    
+   
     <main class="main-content">
         <section class="hero">
             <h1>Bienvenido a Tecnicos y Asociados</h1>
