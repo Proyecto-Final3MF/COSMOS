a37fc09df549ed3e3c2579233ceed3b099fe7c95
VectorAlonne <lucasvargas@hotmail.es>
2025-07-23 17:48:16 -0300

    Solicitudes de Trabajo

---
+++ Controllers/ClienteT.php
@@ -0,0 +1,23 @@
+<?php 
+    class ClienteT {
+        public function crear() {
+            include("views/Cliente/solicitudesT.php");
+        }        
+    }
+
+    public function guardar() {
+
+    }
+
+    public function editar() {
+
+    }
+
+    public funtion actualizar() {
+
+    }
+
+    public function borrar() {
+        
+    }
+?>
