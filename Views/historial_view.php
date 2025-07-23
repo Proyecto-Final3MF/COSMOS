9299489a22f41ffb17ea1a54cbad6f4206805710
AlfreThiago <hiranarizaga@gmail.com>
2025-07-22 23:34:47 -0300

    Css denuevo

--- Views/historial_view.php
+++ Views/historial_view.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Histórico de Modificações</title>
+    <link rel="stylesheet" href="Assets/css/historial.css">
 </head>
 <body>
     <div class="container">
