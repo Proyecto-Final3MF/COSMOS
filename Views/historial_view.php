46a0f5b98f9e747676a2d3b5cc4b92d32ac44619
AlfreThiago <hiranarizaga@gmail.com>
2025-07-23 21:33:45 -0300

    Correcciones en Historial

--- Views/historial_view.php
+++ Views/historial_view.php
@@ -1,10 +1,15 @@
+<?php
+require_once __DIR__ . '/../controllers/HistorialController.php';
+$historialModel = new HistorialModel();
+$historial = $historialModel->getHistorial();
+?>
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Histórico de Modificações</title>
-    <link rel="stylesheet" href="Assets/css/historial.css">
+    <link rel="stylesheet" href="../Assets/css/historial.css">
 </head>
 <body>
     <div class="container">
