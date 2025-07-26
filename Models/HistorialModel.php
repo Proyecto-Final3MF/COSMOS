c5d864283dbd55b2c9e9a69e1e6d31bbb128816e
AlfreThiago <hiranarizaga@gmail.com>
2025-07-25 22:28:48 -0300

    Atualizar o HistorialModel.php

--- Models/HistorialModel.php
+++ Models/HistorialModel.php
@@ -12,7 +12,7 @@ class HistorialModel {
     public function registrarModificacion($usuario_id, $item, $solicitud_id, $obs) {
         $usuario_id_para_db = ($usuario_id === 0 || $usuario_id === null) ? NULL : $usuario_id;
 
-        $query = "INSERT INTO historial (usuario_id, item, solicitud_id, fecha_hora, obs)
+        $query = "INSERT INTO historial (usuario_id, item, item_id, fecha_hora, obs)
                   VALUES (?, ?, ?, NOW(), ?)";
 
         $stmt = $this->conexion->prepare($query);
