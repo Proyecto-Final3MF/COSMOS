21466e7d42aee364116f3b993612afaf277f58e6
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-07-28 16:51:16 -0300

    solo borre comentarios (5mentarios)
    5mentarios

--- Historial.php
+++ Historial.php
@@ -1,13 +1,13 @@
 <?php
 require_once __DIR__ . '/controllers/HistorialController.php';
 
-// Um roteador muito simples. Em uma aplicação maior, você usaria um framework de roteamento.
-$action = $_GET['action'] ?? 'mostrarHistorial'; // Padrão é mostrar o histórico
+
+$action = $_GET['action'] ?? 'mostrarHistorial'; 
 
 $controller = new HistorialController();
 
 if ($action === 'mostrarHistorial') {
     $controller->mostrarHistorial();
 }
-// Adicione outras ações conforme necessário
+
 ?>
