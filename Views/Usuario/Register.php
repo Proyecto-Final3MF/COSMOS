89b0273e909229f59f9ad358acd832ff3c80a0bf
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-07-29 18:24:34 -0300

    no nada

--- Views/Usuario/Register.php
+++ Views/Usuario/Register.php
@@ -4,9 +4,8 @@ session_start();
 
     if (isset($_SESSION['rol']) == ROL_TECNICO or isset($_SESSION['rol']) == ROL_CLIENTE){
         header("Location: index.php?accion=panel");
-    } else {
-        header("Location: index.php?accion=login");
-    }
+    } 
+        
 
 ?>
 <!DOCTYPE html>
