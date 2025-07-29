7e6e54f8bc4094604e7a41ab724e8e6dc37e4ba5
FedeMosegui <agustinmosegui1200@gmail.com>
2025-07-29 19:07:28 -0300

    arreglos

--- Views/includes/header.php
+++ Views/includes/header.php
@@ -23,8 +23,8 @@
         
        
         <div class="action-buttons">
-            <a href="Index.php?accion=login"><button class="btn btn-login">Iniciar sesión</button></a>
-            <a href="Index.php?accion=register"><button class="btn btn-signup pulse">Registrarse</button></a>
+            <a href="../../Index.php?accion=login"><button class="btn btn-login">Iniciar sesión</button></a>
+            <a href="../../Index.php?accion=register"><button class="btn btn-signup pulse">Registrarse</button></a>
         </div>
     </nav>
 </header>
