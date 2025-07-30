491e772145c5991a019f521d696fbb07c5e4c6c7
FedeMosegui <agustinmosegui1200@gmail.com>
2025-07-30 18:17:21 -0300

    thiago deja de escribir en portugues la puta q te pario

--- Views/historial_view.php
+++ Views/historial_view.php
@@ -8,7 +8,7 @@ $historial = $historialModel->getHistorial();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Histórico de Modificações</title>
+    <title>Histórial de Modificaciones</title>
     <link rel="stylesheet" href="../Assets/css/historial.css">
 </head>
 <body>
