88614a728fb8032c0b79e72c74faa1e626306f50
AlfreThiago <hiranarizaga@gmail.com>
2025-08-07 20:19:45 -0300

    Atualizar o Inicio.php

--- Inicio.php
+++ Inicio.php
@@ -8,14 +8,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tecnicos y Asociados</title>
+    <title>C O S M O S</title>
     <link rel="stylesheet" href="./Assets/css/inicio.css">
 </head>
 
 <body>  
     <main class="main-content">
         <section class="hero">
-            <h1>Bienvenido a Tecnicos y Asociados</h1>
+            <h1>Bienvenido a COSMOS</h1>
             <p>Te ayudamos a encontrar un tecnico para arreglar tu dispositivo en tiempo record</p>
             <a href="Index.php"><button class="btn btn-login">Empezar ahora</button></a>
         </section>
