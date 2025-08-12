79a9f6694f0cdb8b9ac9fbafc40632b4fa3350af
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-08-12 20:26:49 -0300

    css.exe
    css para login y register, aun falta un poco pero igual nadie va a leer esto

--- Views/Usuario/Register.php
+++ Views/Usuario/Register.php
@@ -6,19 +6,22 @@
         
 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
+     <link rel="stylesheet" href="./Assets/css/register.css">
 </head>
+<body>
+    
 
 <header>
-    <h2>Registrarse</h2>
+    <h3>Registrarse</h3>
 </header>
 
-<body>
     
     <form method="POST" action="index.php?accion=guardar">
         
