f2bf5ce08e0f944cf732a1a2fe1c827116bdb22c
FedeMosegui <agustinmosegui1200@gmail.com>
2025-08-12 23:07:49 -0300

    yuf

---
+++ Models/ProductoM.php
@@ -0,0 +1,36 @@
+<?php
+require_once("./Config/conexion.php");
+
+class Producto {
+    private $conn;
+
+    public function __construct() {
+        $this->conn = conectar();
+    }
+
+     public function obtenerCategorias(){
+        $sql = "SELECT * FROM producto";
+        $resultado = $this->conn->query($sql);
+        return $resultado->fetch_all(MYSQLI_ASSOC);
+    }
+
+    public function obtenerCategoriaporId($categoria_id) {
+        $categoria_id = (int)$categoria_id;
+        $sql = "SELECT nombre FROM producto WHERE id = $categoria_id LIMIT 1";
+        $result = $this->conn->query($sql);
+        if ($row = $result->fetch_assoc()) {
+            return $row['nombre'];
+        }
+        return null;
+    }
+
+    public function crearP() {
+        $usuario = $this->conn->real_escape_string();
+        $mail = $this->conn->real_escape_string();
+        $contrasena = $this->conn->real_escape_string();
+        
+        $sql = "INSERT INTO usuario () VALUES ()";
+        return $this-conn->query($sql);
+    }
+}
+?>
