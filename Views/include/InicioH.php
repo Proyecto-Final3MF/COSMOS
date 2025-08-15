6cee998e39d45e1b934cde0811e8f0e3a3cc3ee5
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-08-14 22:07:33 -0300

    no nada

--- Views/include/InicioH.php
+++ Views/include/InicioH.php
@@ -1,32 +1,26 @@
 <header>
     <nav class="navbar">
-       
-       
-        
-       
-        <ul class="nav-links" id="nav-links">
+    <ul class="nav-links" id="nav-links">
     <li>
-        <a href="inicio.php">
-            <img src="Assets/imagenes/WhatsApp Image 2025-08-13 at 21.13.14.jpeg" height="50px" alt="logo de la app/pagina cosmos :V">
-        </a>
-        <a href="inicio.php">Inicio</a>
+    <a href="inicio.php">
+    <img src="Assets/imagenes/WhatsApp Image 2025-08-13 at 21.13.14.jpeg" height="50px" alt="logo de la app/pagina cosmos :V">
+    </a>
+    <a href="inicio.php">Inicio</a>
     </li>
     <li><a href="#">Tecnicos</a></li>
     <li><a href="#">Servicios</a></li>
     <li><a href="#">Nosotros</a></li>
     <li><a href="#">Contacto</a></li>
-</ul>
-        
+    </ul>
         
         <div class="search-bar">
-            <input type="text" placeholder="Buscador para buscar" aria-label="Buscar">
-            <button type="submit">Buscar</button>
+        <input type="text" placeholder="Buscador para buscar" aria-label="Buscar">
+        <button type="submit">Buscar</button>
         </div>
         
-       
-        <div class="action-buttons">
-            <a href="Index.php?accion=login"><button class="btn btn-login">Iniciar sesión</button></a>
-            <a href="Index.php?accion=register"><button class="btn btn-signup">Registrarse</button></a>
-        </div>
-    </nav>
+       <div class="action-buttons">
+        <a href="Index.php?accion=login"><button class="btn btn-login">Iniciar sesión</button></a>
+        <a href="Index.php?accion=register"><button class="btn btn-signup">Registrarse</button></a>
+</div>
+</nav>
 </header>
