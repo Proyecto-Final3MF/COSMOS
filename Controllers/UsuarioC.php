5a7cb9b74cf6c1a9a1be4f830ba2584ee348f89e
Franco_Fagundez <francoemanuelfagundezancel3@gmail.com>
2025-08-18 22:57:43 -0300

    cambios

--- Controllers/UsuarioC.php
+++ Controllers/UsuarioC.php
@@ -8,7 +8,7 @@ class UsuarioC {
     public function __construct(){
         $this->historialController = new HistorialController();
     }
-
+    
     public function login() {
         include("views/usuario/login.php");
     }
@@ -57,7 +57,26 @@ class UsuarioC {
             header("Location: index.php?accion=register");
             exit();
         }
+    } 
+
+    public function actualizar() {
+    $id = $_POST['id'];
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    
+    $usuarioM = new Usuario();
+    if ($usuarioM->actualizarU($id, $nombre, $email)) {
+        // Actualiza el nombre en la sesión si es necesario
+        $_SESSION['usuario'] = $nombre;
+        
+        // Redirige al panel del usuario con un mensaje de éxito
+        header("Location: index.php?accion=redireccion&mensaje=Usuario actualizado con éxito.");
+    } else {
+        // Redirige al panel con un mensaje de error
+        header("Location: index.php?accion=redireccion&error=Error al actualizar el usuario.");
     }
+exit();
+}
 
     public function autenticar() {
         $usuario = $_POST['usuario'];
