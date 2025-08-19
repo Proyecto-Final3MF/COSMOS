5bd85fd86b325e25e6823408f1f54401158ffdc2
AlfreThiago <hiranarizaga@gmail.com>
2025-08-18 22:55:48 -0300

    UsuarioC.php

--- Controllers/UsuarioC.php
+++ Controllers/UsuarioC.php
@@ -33,7 +33,7 @@ class UsuarioC {
             $id_user = $usuarioN['id'];
             $obs = "Usuario creado atravez del formulario de registro";
 
-            $this->historialController->registrarModificacion(null, null, 'creo', $usuario, $id_user, $obs);
+            $this->historialController->registrarModificacion(null, null, 'guardo el usuario', $usuario, $id_user, $obs);
 
                 session_start();
                 $_SESSION['usuario'] = $usuarioN['nombre'];
