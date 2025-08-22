52d6754d6cb5ca1b60a0de9298c33e0b5110dbc0
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-08-21 23:12:14 -0300

    peques cambios

--- Views/include/InicioH.php
+++ Views/include/InicioH.php
@@ -10,9 +10,6 @@
     </a>
     <a href="inicio.php">Inicio</a>
     </li>
-    <li><a href="#">Tecnicos</a></li>
-    <li><a href="#">Servicios</a></li>
-    <li><a href="#">Nosotros</a></li>
     <li><a href="#">Contacto</a></li>
     </ul>
         
