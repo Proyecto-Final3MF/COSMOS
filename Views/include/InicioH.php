20fd847fc5d4cb4cc61308a5d4b8b777c6beed54
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-08-26 15:37:42 -0300

    cambios al css

--- Views/include/InicioH.php
+++ Views/include/InicioH.php
@@ -1,23 +1,17 @@
 <header>
     <nav class="navbar">
-       
+        <a href="inicio.php" class="logo-link">
+            <img src="Assets/imagenes/WhatsApp Image 2025-08-13 at 21.13.14.jpeg" height="50px" alt="logo de la app/pagina cosmos :V">
+        </a>
         
-       
         <ul class="nav-links" id="nav-links">
-    <li>
-    <a href="inicio.php">
-    <img src="Assets/imagenes/WhatsApp Image 2025-08-13 at 21.13.14.jpeg" height="50px" alt="logo de la app/pagina cosmos :V">
-    </a>
-    <a href="inicio.php">Inicio</a>
-    </li>
-    <li><a href="#">Contacto</a></li>
-    </ul>
+            <li><a href="inicio.php">Inicio</a></li>
+            <li><a href="#">Contacto</a></li>
+        </ul>
         
-       
-        
-       <div class="action-buttons">
-        <a href="Index.php?accion=login"><button class="btn btn-login">Iniciar sesión</button></a>
-        <a href="Index.php?accion=register"><button class="btn btn-signup">Registrarse</button></a>
-</div>
-</nav>
+        <div class="action-buttons">
+            <a href="Index.php?accion=login"><button class="btn btn-login">Iniciar sesión</button></a>
+            <a href="Index.php?accion=register"><button class="btn btn-signup">Registrarse</button></a>
+        </div>
+    </nav>
 </header>
