1a42e89615ffca8a0c05840e3b1f22c67aad1458
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-09-01 07:17:04 -0300

    css.javascript

--- Views/include/TH.php
+++ Views/include/TH.php
@@ -3,9 +3,8 @@
          <a href="inicio.php" class="logo-link">
             <img src="Assets/imagenes/logoNueva.png" height="50px" alt="logo de la app/pagina cosmos :V"> 
         </a>
-       <h1>Bienvenido Tecnico <?= htmlspecialchars($_SESSION['usuario']) ?></h1>
-       <a href=""><button class="btn btn-boton">no nada</button></a>
-       <a href=""><button class="btn btn-boton">testeando</button></a>
+       <h2>Bienvenido Tecnico <?= htmlspecialchars($_SESSION['usuario']) ?></h2>
+      
        <a href="Index.php?accion=logout"><button class="btn btn-boton">Cerrar Sesion</button></a>
     </nav>
 </header>
