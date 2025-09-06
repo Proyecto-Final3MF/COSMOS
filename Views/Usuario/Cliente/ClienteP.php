7e41d4490c56fef14f7be95f50de9fc8bb0bb91e
AlfreThiago <hiranarizaga@gmail.com>
2025-09-05 23:29:47 -0300

    ClienteP.php

--- Views/Usuario/Cliente/ClienteP.php
+++ Views/Usuario/Cliente/ClienteP.php
@@ -1,5 +1,6 @@
 <?php
-    if (isset($_SESSION['rol']) != ROL_CLIENTE) {
+         if (isset($_SESSION['rol']) == ROL_CLIENTE) {
+    } elseif (isset($_SESSION['rol']) == ROL_TECNICO){
         header("Location: index.php?accion=redireccion");
     } else {
         header("Location: index.php?accion=login");
