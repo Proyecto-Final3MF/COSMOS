02aa1bd96f7354297ea6705908b1a1da431eb39e
AlfreThiago <hiranarizaga@gmail.com>
2025-09-09 00:35:28 -0300

    Update Error.php

--- Error.php
+++ Error.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página de Erro</title>
-    <link rel="stylesheet" href="/Assets/css/error.css">
+    <link rel="stylesheet" href="./Assets/css/error.css">
 </head>
 <body>
     <video autoplay muted loop playsinline id="background-video">
