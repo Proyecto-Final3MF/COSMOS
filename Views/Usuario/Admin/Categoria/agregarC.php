9892462fed5bf22539c395e973aa6a77156b1952
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-09-10 22:26:23 -0300

    css para editar y crear categoria admin

--- Views/Usuario/Admin/Categoria/agregarC.php
+++ Views/Usuario/Admin/Categoria/agregarC.php
@@ -4,15 +4,20 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+   
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Agregar Categoria</title>
+    <link rel="stylesheet" href="./Assets/css/Formulario.css">
 </head>
 <body>
+    <section>
+    <h3>Crear Nueva Categoria</h3>
     <form action="index.php?accion=guardarC" method="post">
-        <label for="nombre">Nueva Categoria: </label><br>
+        <label for="nombre">Nueva Categoria: </label>
         <input type="text" id="nombre" name="nombre"/>
-        <button name="Agregar">Agregar Categoria</button>
+        <button type="submit">Agregar Categoria</button>
     </form>
+    </section>
 </body>
 </html>
