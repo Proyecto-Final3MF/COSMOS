f6b5b64dbc63345a3ee202e5f432e575644452eb
AlfreThiago <hiranarizaga@gmail.com>
2025-09-17 19:56:20 -0300

    Reorganizando tabs

--- Views/include/InicioH.php
+++ Views/include/InicioH.php
@@ -6,19 +6,18 @@
         </a>
         
 
-  <select id="language-select">
+    <select id="language-select">
         <option value="es">Español</option>
         <option value="en">English</option>
     </select>
 
-        <ul class="nav-links" id="nav-links">
-            <li><a href="inicio.php" data-translate="startPage">Inicio</a></li>
-            <li><a href="./Views/Usuario/contacto.php" data-translate="contact">Contacto</a></li>
-        </ul>
+    <ul class="nav-links" id="nav-links">
+        <li><a href="inicio.php" data-translate="startPage">Inicio</a></li>
+        <li><a href="./Views/Usuario/contacto.php" data-translate="contact">Contacto</a></li>
+    </ul>
         
-         <class="action-buttons">
-            <a href="Index.php?accion=login"><button class="btn btn-boton" data-translate="signin">Iniciar sesión</button></a>
-            <a href="Index.php?accion=register"><button class="btn btn-boton" data-translate="signup">Registrarse</button></a>
-        
+    <class="action-buttons">
+        <a href="Index.php?accion=login"><button class="btn btn-boton" data-translate="signin">Iniciar sesión</button></a>
+        <a href="Index.php?accion=register"><button class="btn btn-boton" data-translate="signup">Registrarse</button></a>
     </nav>
 </header>
