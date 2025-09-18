6e16d32633fb4c606dd0c138595b84a08a796aaa
AlfreThiago <hiranarizaga@gmail.com>
2025-09-18 20:52:16 -0300

    Antes de mezclar con main, actualizen sus ramas

--- Views/include/CH.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <nav class="navbar">
-      <a href="inicio.php" class="logo-link">
-      <img src="Assets/imagenes/logoNueva.png" height="50px" alt="logo de la app/pagina cosmos :V">
-        <h2>Bienvenido <?= htmlspecialchars($_SESSION['usuario']) ?></h2>
-       <a href="Index.php?accion=logout">Cerrar sesion</a>
-    </nav>
-</header>
