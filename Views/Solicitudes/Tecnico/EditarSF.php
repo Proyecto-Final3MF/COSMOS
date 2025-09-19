7c847739f4459ea6de41fa13f194062f9f1701a1
FedeMosegui <agustinmosegui1200@gmail.com>
2025-09-18 23:17:13 -0300

    adsad

---
+++ Views/Solicitudes/Tecnico/EditarSF.php
@@ -0,0 +1,32 @@
+<?php
+    include_once("./Views/include/UH.php");
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Editar Usuario</title>
+    <link rel="stylesheet" href="./Assets/css/Formulario.css">
+</head>
+<body>
+    <div class="contenedor-formulario">
+    <section>
+<h3>Editar Usuario</h3>
+
+<form method="POST" action="Index.php?accion=actualizarU">
+    
+    <input type="hidden" name="id" value="<?= $datos['id'] ?>">
+        
+    <p> Nombre: </p>  <input type="text" name="nombre" value="<?= $datos['nombre'] ?>"><br>
+        
+   <p> Email: </p> <input type="mail" name="email" value="<?= $datos['email'] ?>"><br>
+       
+    <input type="submit" value="Guardar cambios">
+
+</form>
+</section>
+</div>
+</body>
+</html> 
