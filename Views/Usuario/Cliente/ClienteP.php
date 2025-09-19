cca534c5842548979ab8bb06df9eaf0c15aa8d91
FedeMosegui <agustinmosegui1200@gmail.com>
2025-09-18 23:47:02 -0300

    as;dk;

--- Views/Usuario/Cliente/ClienteP.php
+++ Views/Usuario/Cliente/ClienteP.php
@@ -23,7 +23,6 @@ require_once ("./Views/include/UH.php");
     <a href="index.php?accion=listarSLU"><button class="btn btn-boton">Ver Mis Solicitudes Sin Asignar</button></a>
     <a href="index.php?accion=listarSA"><button class="btn btn-boton">Ver Mis Solicitudes Aceptadas</button></a>
     <a href="index.php?accion=listarSFU"><button class="btn btn-boton">Ver Mis Solicitudes Terminadas</button></a>
-    
     </div>
 </div>
 </body>
