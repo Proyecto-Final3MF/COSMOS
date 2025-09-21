62ef0cb5fba217c9f4d36154f101971d0e871c6a
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-09-20 21:48:41 -0300

    Update ListadoTL.php

--- Views/Solicitudes/Tecnico/ListadoTL.php
+++ Views/Solicitudes/Tecnico/ListadoTL.php
@@ -56,7 +56,7 @@ require_once ("./Views/include/UH.php");
   } else {
     ?>
     <tr>
-      <td colspan="7">No hay solicitudes disponibles en este momento.<br><br><a href="index.php?accion=listarSA"><button class="btn btn-boton2">Ver solicitudes aceptadas</button></a></td>
+      <td colspan="7">No hay solicitudes disponibles en este momento<br><br><a href="index.php?accion=listarSA"><button class="btn btn-boton2">Ver solicitudes aceptadas</button></a></td>
     </tr>
     <?php
   }
