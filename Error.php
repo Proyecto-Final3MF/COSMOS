2543d77cc6945175558b3ebdcd1ff5a84a5de7ae
AlfreThiago <hiranarizaga@gmail.com>
2025-09-23 19:39:29 -0300

    Squashed commit of the following:
    commit 6ec85e576061f33d98d2537879b8c3738e5b0ad0
    Author: AlfreThiago <hiranarizaga@gmail.com>
    Date:   Tue Sep 23 19:33:32 2025 -0300
    
        Mas cambios a error.php y error.css

--- Error.php
+++ Error.php
@@ -10,10 +10,10 @@
     <div class="noise"></div>
 <div class="overlay"></div>
 <div class="terminal">
-  <h1>Error <span class="errorcode">404</span></h1>
-  <p class="output">Es posible que la página que estás buscando haya sido eliminada, no exista, haya cambiado de nombre o no esté disponible temporalmente.</p>
-  <p class="output">Podes intentar volver a <a href="inicio.php">Pagina Inicial</a>.</p>
-  <p class="output">Gracias por la comprensión.</p>
+  <h1 class="h1_error">Error <span class="errorcode">404</span></h1>
+  <p class="output">> Es posible que la página que estás buscando haya sido eliminada, no exista, haya cambiado de nombre o no esté disponible temporalmente.</p>
+  <p class="output">> Podes intentar volver a <a class="errorLink" href="inicio.php">[Pagina Inicial]</a>.</p>
+  <p class="output">> Gracias por la comprensión.</p>
 </div>
 <script src="Assets/js/trancicion.js"></script>
 </body>
