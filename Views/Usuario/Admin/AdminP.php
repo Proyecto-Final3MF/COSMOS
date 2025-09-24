c923cf6c52b1952c1e129480d5827c46246e8294
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-09-23 23:33:48 -0300

    Css juntados ahora todas las paginas comparten un css el cual es Main.css

--- Views/Usuario/Admin/AdminP.php
+++ Views/Usuario/Admin/AdminP.php
@@ -14,7 +14,7 @@ require_once ("./Views/include/UH.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel de Admin</title>
-    <link rel="stylesheet" href="./Assets/css/inicio.css"> </head>
+    <link rel="stylesheet" href="./Assets/css/Main.css"> </head>
 <body>
     <main>
         <h2>Aquí podrás gestionar tus tareas como Admin.</h2>
--- Views/include/UH.php
+++ Views/include/UH.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 ?>
 <header>
-    <link rel="stylesheet" href="./Assets/css/inicio.css">
+    <link rel="stylesheet" href="./Assets/css/main.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 
     <nav class="navbar">
