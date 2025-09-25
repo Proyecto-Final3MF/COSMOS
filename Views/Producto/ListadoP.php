aa811c63cf9f04de3198a6d18b34dc4840a29658
AlfreThiago <hiranarizaga@gmail.com>
2025-09-25 02:26:21 -0300

    Boton robado

--- Views/Producto/ListadoP.php
+++ Views/Producto/ListadoP.php
@@ -14,7 +14,7 @@ require_once ("./Views/include/UH.php");
     <br>
     <h2>Tus Productos</h2>
 <div class="botones-container">
-    <a href="index.php?accion=formularioP"><button class="btn btn-boton">Crear Nuevo Producto</button></a>
+    <a href="index.php?accion=formularioP"><button class="btn-agregar btn btn-boton2"><img src="Assets/imagenes/plus.png" alt="agregar" width="45"></button></a>
 </div>
 
 <table>
