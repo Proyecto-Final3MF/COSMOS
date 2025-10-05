82fe217457c9cbfbab91d895702fdba2448ba1af
AlfreThiago <hiranarizaga@gmail.com>
2025-10-04 21:26:50 -0300

    Update listadoSA.php

--- Views/Solicitudes/listadoSA.php
+++ Views/Solicitudes/listadoSA.php
@@ -9,18 +9,6 @@ require_once ("./Views/include/UH.php");
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sus Solicitudes</title>
     <link rel="stylesheet" href="./Assets/css/Main.css" />
-    <style>
-        .list-item td:last-child .botones-container {
-            display: flex;
-            gap: 10px;
-            justify-content: center;
-            align-items: center;
-        }
-
-        .list-item td:last-child .botones-container a {
-            display: center;
-        }
-    </style>
 </head>
 <body>
     <br>
