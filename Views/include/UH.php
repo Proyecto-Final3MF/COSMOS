9114900b5bcf27860868ce66a8ebd4d5fe2b799b
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-10-05 05:14:28 -0300

    botones lindos y con personalidad

--- Views/include/UH.php
+++ Views/include/UH.php
@@ -5,6 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 ?>
 <header>
     <link rel="stylesheet" href="./Assets/css/main.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
 
     <nav class="navbar">
