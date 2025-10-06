4e7694dd6608768cbb1493c4eb9a1d5accd58ef5
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-10-06 03:04:12 -0300

    volver mejorado en todo

--- Views/Usuario/Admin/Categoria/agregarC.php
+++ Views/Usuario/Admin/Categoria/agregarC.php
@@ -17,10 +17,10 @@ require_once ("./Views/include/UH.php");
 </head>
 
 <body>
-    <div class="btn-volver-container">
-    <a href="index.php?accion=redireccion" class="btn-volver">
-        <i class="fa fa-arrow-left"></i> Volver
-    </a>
+   <div class="btn-volver-container">
+    <button class="btn-volver" id="btnVolver">
+    <i class="fa fa-arrow-left"></i> Volver
+</button>
 </div>
     <div class="contenedor-formulario">
     <section>
--- Views/Usuario/Admin/listarU.php
+++ Views/Usuario/Admin/listarU.php
@@ -19,9 +19,9 @@ require_once("./Views/include/UH.php");
 <h2 class="fade-slide" >Listado de todos los Usuarios</h2>
 
 <div class="btn-volver-container">
-    <a href="index.php?accion=redireccion" class="btn-volver">
-        <i class="fa fa-arrow-left"></i> Volver
-    </a>
+    <button class="btn-volver" id="btnVolver">
+    <i class="fa fa-arrow-left"></i> Volver
+</button>
 </div>
 
 <form action="index.php" class="ordenar-form">
