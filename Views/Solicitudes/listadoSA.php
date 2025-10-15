51e20b16b5cf5e8d45a70b00ab2624a6d99bec64
VectorAlonne <lucasvargas@hotmail.es>
2025-10-14 22:48:55 -0300

    cambio a listadoSA sobre el chat

--- Views/Solicitudes/listadoSA.php
+++ Views/Solicitudes/listadoSA.php
@@ -58,10 +58,19 @@ require_once("./Views/include/UH.php");
                                         <img src="Assets/imagenes/pen.png" alt="editar" width="45">
                                     </a>
                                 <?php endif; ?>
-                                <a href="index.php?accion=mostrarConversacion&usuario_id=<?= $resultado['tecnico_id'] ?? $resultado['cliente_id'] ?? $resultado['usuario_id']; ?>"
-                                    class="btn btn-boton2">
-                                    <img src="Assets/imagenes/chat.png" alt="chat" width="40">
-                                </a>
+
+                                <?php foreach ($resultados as $resultado): ?>
+                                    <?php
+                                    $usuarioDestino = ($_SESSION['rol'] == 'tecnico')
+                                        ? $resultado['cliente_id']
+                                        : $resultado['tecnico_id'];
+                                    ?>
+                                    <a href="index.php?accion=mostrarChat&usuario_id=<?= $usuarioDestino ?>"
+                                        class="btn btn-boton2">
+                                        <img src="Assets/imagenes/chat.png" alt="chat" width="40">
+                                    </a>
+                                <?php endforeach; ?>
+
                                 <a href="index.php?accion=cancelarS&id_solicitud=<?= $resultado['id']; ?>"
                                     onclick="return confirm('¿Estás seguro de que quieres cancelar esta solicitud?');"
                                     class="btn btn-boton2 danger">
