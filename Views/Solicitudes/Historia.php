658bdd487d325f01765620033d538846b26f6cd5
AlfreThiago <hiranarizaga@gmail.com>
2025-10-15 22:50:17 -0300

    Refresh refresheado

--- Views/Solicitudes/Historia.php
+++ Views/Solicitudes/Historia.php
@@ -1,3 +1,4 @@
+<meta http-equiv="refresh" content="10">
 <?php
 foreach ($resultados as $evento) {
     echo $evento['evento']. " (". date('H:i:s d/m/Y', strtotime($evento['fecha_hora'])). ")<br>";
