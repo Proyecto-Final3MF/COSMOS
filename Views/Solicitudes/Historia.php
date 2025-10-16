80fbd8a486c7ce9f00b7b294006d1e0f75e35224
AlfreThiago <hiranarizaga@gmail.com>
2025-10-16 02:21:10 -0300

    Update Historia.php

--- Views/Solicitudes/Historia.php
+++ Views/Solicitudes/Historia.php
@@ -3,16 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Assets/css/Main.css"/>
     <meta http-equiv="refresh" content="30">
     <title>Linea del Tiempo</title>
 </head>
 <body>
     <h2>Linea del Tiempo</h2>
     <?php foreach ($resultados as $evento): ?>
-    <div class="list-item">
     <p> <?php echo $evento['evento']. " (". date('H:i:s d/m/Y', strtotime($evento['fecha_hora'])). ")"; ?>
     </p>
-    </div>
     <?php endforeach; ?>
 </body>
 </html>
