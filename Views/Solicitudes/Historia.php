ba962afc8bbca76b93cef4aa9d0d53f1eca7ce49
AlfreThiago <hiranarizaga@gmail.com>
2025-10-16 00:59:38 -0300

    Mejora pagina historia solicitud

--- Views/Solicitudes/Historia.php
+++ Views/Solicitudes/Historia.php
@@ -1,5 +1,18 @@
-<meta http-equiv="refresh" content="30">
-<?php
-foreach ($resultados as $evento) {
-    echo $evento['evento']. " (". date('H:i:s d/m/Y', strtotime($evento['fecha_hora'])). ")<br>";
-}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="30">
+    <title>Linea del Tiempo</title>
+</head>
+<body>
+    <h2>Linea del Tiempo</h2>
+    <?php foreach ($resultados as $evento): ?>
+    <div class="list-item">
+    <p> <?php echo $evento['evento']. " (". date('H:i:s d/m/Y', strtotime($evento['fecha_hora'])). ")"; ?>
+    </p>
+    </div>
+    <?php endforeach; ?>
+</body>
+</html>
