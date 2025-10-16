bea1293fa18833ab2cecddccd333e05890a22330
AlfreThiago <hiranarizaga@gmail.com>
2025-10-16 02:35:59 -0300

    Pagina historia solicitud problema solucionado

--- Views/Solicitudes/Historia.php
+++ Views/Solicitudes/Historia.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,9 +9,17 @@
 </head>
 <body>
     <h2>Linea del Tiempo</h2>
+    <div class="container3">
     <?php foreach ($resultados as $evento): ?>
+    <div class="list-item">
     <p> <?php echo $evento['evento']. " (". date('H:i:s d/m/Y', strtotime($evento['fecha_hora'])). ")"; ?>
     </p>
+    </div>
     <?php endforeach; ?>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="Assets/js/paginacion.js"></script>
+    <script src="Assets/js/trancicion.js"></script>
+    <script src="Assets/js/botonvolver.js"></script>
 </body>
 </html>
