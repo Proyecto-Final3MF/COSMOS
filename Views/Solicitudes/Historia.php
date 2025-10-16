cc22a2be093c6f5cfdf57d31b2d757591e8d572b
AlfreThiago <hiranarizaga@gmail.com>
2025-10-15 22:56:08 -0300

    Refresh mejor

--- Views/Solicitudes/Historia.php
+++ Views/Solicitudes/Historia.php
@@ -1,4 +1,4 @@
-<meta http-equiv="refresh" content="10">
+<meta http-equiv="refresh" content="30">
 <?php
 foreach ($resultados as $evento) {
     echo $evento['evento']. " (". date('H:i:s d/m/Y', strtotime($evento['fecha_hora'])). ")<br>";
--- Views/Solicitudes/Tecnico/ListadoTL.php
+++ Views/Solicitudes/Tecnico/ListadoTL.php
@@ -7,7 +7,7 @@ require_once ("./Views/include/UH.php");
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="refresh" content="10">
+  <meta http-equiv="refresh" content="30">
   <title>Solicitudes Libres</title>
   <link rel="stylesheet" href="./Assets/css/Main.css" />
 </head>
