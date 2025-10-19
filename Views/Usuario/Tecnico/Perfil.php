875fcb7db3c0f19efcb7a4a3d9c40ac39763597d
AlfreThiago <hiranarizaga@gmail.com>
2025-10-19 19:32:22 -0300

    Me quiero matar

--- Views/Usuario/Tecnico/Perfil.php
+++ Views/Usuario/Tecnico/Perfil.php
@@ -8,17 +8,17 @@ foreach ($ReviewsTecnico as $review):
 echo $review['comentario']." ".$review['fecha_creacion']." ".$review['fecha_edicion'];
 
 ?>
-<fieldset class="rate">
- <input type="radio" id="rating10" name="rating" value="10" <?= ($review['rating'] == 10) ? 'checked' : '' ?> /><label for="rating10" title="5 stars"></label>
-        <input type="radio" id="rating9" name="rating" value="9" <?= ($review['rating'] == 9) ? 'checked' : '' ?> /><label class="half" for="rating9" title="4 1/2 stars"></label>
-        <input type="radio" id="rating8" name="rating" value="8" <?= ($review['rating'] == 8) ? 'checked' : '' ?> /><label for="rating8" title="4 stars"></label>
-        <input type="radio" id="rating7" name="rating" value="7" <?= ($review['rating'] == 7) ? 'checked' : '' ?> /><label class="half" for="rating7" title="3 1/2 stars"></label>
-        <input type="radio" id="rating6" name="rating" value="6" <?= ($review['rating'] == 6) ? 'checked' : '' ?> /><label for="rating6" title="3 stars"></label>
-        <input type="radio" id="rating5" name="rating" value="5" <?= ($review['rating'] == 5) ? 'checked' : '' ?> /><label class="half" for="rating5" title="2 1/2 stars"></label>
-        <input type="radio" id="rating4" name="rating" value="4" <?= ($review['rating'] == 4) ? 'checked' : '' ?> /><label for="rating4" title="2 stars"></label>
-        <input type="radio" id="rating3" name="rating" value="3" <?= ($review['rating'] == 3) ? 'checked' : '' ?> /><label class="half" for="rating3" title="1 1/2 stars"></label>
-        <input type="radio" id="rating2" name="rating" value="2" <?= ($review['rating'] == 2) ? 'checked' : '' ?> /><label for="rating2" title="1 star"></label>
-        <input type="radio" id="rating1" name="rating" value="1" <?= ($review['rating'] == 1) ? 'checked' : '' ?> /><label class="half" for="rating1" title="1/2 star"></label>
+<fieldset class="rate ratings-list">
+    <input type="radio" id="rating10" name="rating" value="10" <?= ($review['rating']*2 == 10) ? 'checked' : '' ?> /><label for="rating10" title="5 stars"></label>
+    <input disabled type="radio" id="rating9" name="rating" value="9" <?= ($review['rating']*2 == 9) ? 'checked' : '' ?> /><label class="half" for="rating9" title="4 1/2 stars"></label>
+    <input disabled type="radio" id="rating8" name="rating" value="8" <?= ($review['rating']*2 == 8) ? 'checked' : '' ?> /><label for="rating8" title="4 stars"></label>
+    <input disabled type="radio" id="rating7" name="rating" value="7" <?= ($review['rating']*2 == 7) ? 'checked' : '' ?> /><label class="half" for="rating7" title="3 1/2 stars"></label>
+    <input disabled type="radio" id="rating6" name="rating" value="6" <?= ($review['rating']*2 == 6) ? 'checked' : '' ?> /><label for="rating6" title="3 stars"></label>
+    <input disabled type="radio" id="rating5" name="rating" value="5" <?= ($review['rating']*2 == 5) ? 'checked' : '' ?> /><label class="half" for="rating5" title="2 1/2 stars"></label>
+    <input disabled type="radio" id="rating4" name="rating" value="4" <?= ($review['rating']*2 == 4) ? 'checked' : '' ?> /><label for="rating4" title="2 stars"></label>
+    <input disabled type="radio" id="rating3" name="rating" value="3" <?= ($review['rating']*2 == 3) ? 'checked' : '' ?> /><label class="half" for="rating3" title="1 1/2 stars"></label>
+    <input disabled type="radio" id="rating2" name="rating" value="2" <?= ($review['rating']*2 == 2) ? 'checked' : '' ?> /><label for="rating2" title="1 star"></label>
+    <input disabled type="radio" id="rating1" name="rating" value="1" <?= ($review['rating']*2 == 1) ? 'checked' : '' ?> /><label class="half" for="rating1" title="1/2 star"></label>
 </fieldset>
 <?php endforeach; ?>
 <script src="Assets/js/trancicion.js"></script>
