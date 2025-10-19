c0d83e145be9c8848c6ebd13d94152a117e99c81
AlfreThiago <hiranarizaga@gmail.com>
2025-10-19 18:53:05 -0300

    Falta css

--- Views/Usuario/Tecnico/Perfil.php
+++ Views/Usuario/Tecnico/Perfil.php
@@ -1,1 +1,4 @@
-a
+<?php
+echo $DatosTecnico['nombre']." ". $DatosTecnico['email']." ".$DatosTecnico['cant_review']." ".$DatosTecnico['promedio'];
+?>
+<img src="<?=htmlspecialchars($DatosTecnico['foto_perfil'])?>" alt=""/>
