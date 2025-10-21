c280dc7d5edca54a5b5a85b496913a0f506e747e
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-10-20 23:33:07 -0300

    foto en la pestaña

--- Inicio.php
+++ Inicio.php
@@ -8,6 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>C O S M O S</title>
+    <link rel="icon" type="image/png" href="Assets/imagenes/logonueva.png">
     <link rel="stylesheet" href="./Assets/css/Main.css">
 </head>
 <body>
