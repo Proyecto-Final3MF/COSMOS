b774183de38386b1afa147944739ef17fa0c147a
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-10-22 05:57:27 -0300

    arreglitos arreglosos

--- Views/include/UH.php
+++ Views/include/UH.php
@@ -1,4 +1,8 @@
 <?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 require_once(dirname(__DIR__, 2) . '/Controllers/NotificacionC.php');
 
 $notifC = new NotificacionC();
