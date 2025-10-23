d570d5c3a7600f3eeee1ccfe7e1b9fd799002a5d
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-10-22 22:26:06 -0300

    cambitos cambiosos (CSS)

--- Views/Producto/EditarP.php
+++ Views/Producto/EditarP.php
@@ -56,5 +56,6 @@ require_once ("./Views/include/UH.php");
 </div>
 <script src="Assets/js/imagenformulario.js"></script>
 <script src="Assets/js/trancicion.js"></script>
+<script src="Assets/js/botonvolver.js"></script>
 </body>
 </html>
