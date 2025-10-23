1ddc5a2890b9eab6861bbfc65fd743ba869a55f5
AlfreThiago <hiranarizaga@gmail.com>
2025-10-23 19:16:09 -0300

    Update listarC.php

--- Views/Usuario/Admin/Categoria/listarC.php
+++ Views/Usuario/Admin/Categoria/listarC.php
@@ -28,7 +28,7 @@ require_once("./Views/include/UH.php");
 </div>
 <form action="index.php" class="ordenar-form">
     <label for="search">Buscar: </label>
-    <input type="text" id="search" name="search" placeholder="Buscar Producto" value="<?php echo htmlspecialchars($_GET['search'] ?? ''); ?>">
+    <input type="text" id="search" name="search" placeholder="Buscar Categoria" value="<?php echo htmlspecialchars($_GET['search'] ?? ''); ?>">
     <label for="orden">Ordenar por:</label>
     <input type="hidden" name="accion" value="listarC">
     <select name="orden" id="orden">
