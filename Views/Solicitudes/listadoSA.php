d88aa8f081c91764f70367e45486dbebff3c72de
VectorAlonne <lucasvargas@hotmail.es>
2025-10-23 15:53:33 -0300

    avance

--- Views/Solicitudes/listadoSA.php
+++ Views/Solicitudes/listadoSA.php
@@ -54,10 +54,10 @@ require_once("./Views/include/UH.php");
                         <td><?= htmlspecialchars($resultado['prioridad']); ?></td>
                         <td><?= htmlspecialchars($resultado['descripcion']); ?></td>
                         <?php if ($_SESSION['rol'] == ROL_CLIENTE): ?>
-                           <td>
-                            <a title="Perfil del Tecnico" href="index.php?accion=PerfilTecnico&id=<?= $resultado['id_tecnico'] ?>" class="btn btn-perfil-tecnico">
-                            <i class="fa fa-user"></i> <?= htmlspecialchars($resultado['nombre_tecnico'] ?? 'No asignado'); ?>
-                            </a>
+                            <td>
+                                <a title="Perfil del Tecnico" href="index.php?accion=PerfilTecnico&id=<?= $resultado['id_tecnico'] ?>" class="btn btn-perfil-tecnico">
+                                    <i class="fa fa-user"></i> <?= htmlspecialchars($resultado['nombre_tecnico'] ?? 'No asignado'); ?>
+                                </a>
                             </td>
                         <?php elseif ($_SESSION['rol'] == ROL_TECNICO): ?>
                             <td><?= htmlspecialchars($resultado['nombre_cliente']); ?></td>
@@ -73,18 +73,17 @@ require_once("./Views/include/UH.php");
                                 <?php endif; ?>
 
                                 <?php
-                                $usuarioDestino = 0;
-                                if (isset($_SESSION['rol'])) {
-                                    if ($_SESSION['rol'] == ROL_TECNICO) {
-                                        $usuarioDestino = $resultado['cliente_id'];
-                                    } elseif ($_SESSION['rol'] == ROL_CLIENTE || $_SESSION['rol'] == 1) { // Asumiendo rol 1 es Admin
-                                        $usuarioDestino = $resultado['tecnico_id'];
-                                    }
-                                }
+                                $usuarioLogueado = $_SESSION['id'];
+
+                                $usuarioDestino = isset($resultado['id_tecnico'])
+                                    ? $resultado['id_tecnico']
+                                    : $resultado['id_usuario'];
+
+                                $solicitudId = $resultado['id'];
                                 ?>
 
                                 <?php if ($usuarioDestino): ?>
-                                    <a href="index.php?accion=mostrarChat&usuario_id=<?= $usuario_id ?>&id_solicitud=<?= $solicitud_id ?>"
+                                    <a href="index.php?accion=mostrarChat&usuario_id=<?= $usuarioDestino ?>&id_solicitud=<?= $solicitudId ?>"
                                         class="icon-btn chat">
                                         <i class="fa fa-comments"></i>
                                     </a>
