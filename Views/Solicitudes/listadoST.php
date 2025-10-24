615f5c91545d9bc904d443ada342ac1b81258af5
AlfreThiago <hiranarizaga@gmail.com>
2025-10-24 20:23:14 -0300

    Rating en listarST

--- Views/Solicitudes/listadoST.php
+++ Views/Solicitudes/listadoST.php
@@ -35,6 +35,7 @@ require_once ("./Views/include/UH.php");
                     <th>Cliente</th>
                 <?php endif; ?>
                 <th>Estado</th>
+                <th>Calificación</th>
                 <th>Fecha de Creacion</th>
                 <th>Acciones</th>
             </tr>
@@ -63,6 +64,7 @@ require_once ("./Views/include/UH.php");
                             <td><?= htmlspecialchars($resultado['nombre_cliente']); ?></td>
                         <?php endif; ?>
                     <td><?= htmlspecialchars($resultado['estado_nombre']); ?></td>
+                    <td><?= htmlspecialchars($resultado['rating']); ?></td>
                     <td><?= htmlspecialchars(date('d/m/Y H:i:s ', strtotime($resultado['fecha_creacion']))); ?></td>
                 
                 <td>
