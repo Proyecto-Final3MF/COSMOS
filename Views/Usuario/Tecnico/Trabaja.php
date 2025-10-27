5334ff1d7d74fea1732596eef1ddfd7ba73b8547
FedeMosegui <agustinmosegui1200@gmail.com>
2025-10-27 20:55:04 -0300

    dslasdfasdfasdf

--- Views/Usuario/Tecnico/Trabaja.php
+++ Views/Usuario/Tecnico/Trabaja.php
@@ -3,5 +3,16 @@
 ?>
 <br>
 
-<h1>Bienvenido</h1>
-<p>para trabajar con nosotros porfavor envianos tu email para poder entrar en contacto</p>
+<h1 class="fade-label">Bienvenido</h1>
+<p class="fade-label">para trabajar con nosotros porfavor envianos tu email para poder entrar en contacto</p>
+
+<div class="contenedor-formulario">
+    <section class="formularios999">
+        <form method="POST" action="Index.php?accion=mail">
+            <p class="fade-label">Correo electrónico</p>
+            <input type="email" class="form-control" id="usuario" name="usuario" autocomplete="off" required>
+
+            <button class="button" type="submit">Enviar</button>
+        </form>
+    </section>
+</div>
