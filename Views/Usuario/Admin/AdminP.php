644e168102ef14f34162c4cf7b1dc64f48236c68
AlfreThiago <alfrethiagoc@gmail.com>
2025-10-27 20:54:18 -0300

    Cambios a historial q deberia haber hecho a mil años atras

--- Views/Usuario/Admin/AdminP.php
+++ Views/Usuario/Admin/AdminP.php
@@ -1,5 +1,5 @@
 <?php 
-if (!isset($_SESSION['rol']) || $_SESSION['rol'] != ROL_ADMIN) { 
+if (!isset($_SESSION['rol']) || $_SESSION['rol'] != 3) { 
     header("Location: Index.php?accion=redireccion"); 
     exit(); 
 } 
