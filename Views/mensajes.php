7fecb6bd5be90a68194e284a4ec1418be83fd740
VectorAlonne <lucasvargas@hotmail.es>
2025-10-27 15:33:48 -0300

    Merge branch 'Test'

--- Views/mensajes.php
+++ Views/mensajes.php
@@ -1,14 +1,7 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-
-?>
-<link rel="stylesheet" href="Assets/css/chatCSS.css">
 <?php if (!empty($mensajes)): ?>
     <?php foreach ($mensajes as $m): ?>
         <?php
-            $nombre = ($m['usuario_id'] == $_SESSION['id']) ? 'Tú' : ($m['emisor'] ?? $m['receptor'] ?? '???');
+        $nombre = ($m['usuario_id'] == $_SESSION['id']) ? 'Tú' : ($m['emisor'] ?? $m['receptor'] ?? '???');
         ?>
         <div class="mensaje">
             <p class="texto">
@@ -20,5 +13,3 @@ if (session_status() === PHP_SESSION_NONE) {
 <?php else: ?>
     <p class="sin-mensajes">No hay mensajes aún.</p>
 <?php endif; ?>
-
-<script src="Assets/js/trancicion.js"></script>
