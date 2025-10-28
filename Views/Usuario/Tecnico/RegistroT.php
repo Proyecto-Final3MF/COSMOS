e3aa45afa494ca60a27cf75c3e5143c2e3438f83
FedeMosegui <agustinmosegui1200@gmail.com>
2025-10-28 15:07:32 -0300

    mas comits al pedo

--- Views/Usuario/Tecnico/RegistroT.php
+++ Views/Usuario/Tecnico/RegistroT.php
@@ -1,98 +1,90 @@
 <?php
 
 if (isset($_SESSION['rol']) && in_array($_SESSION['rol'], [1, 2, 3])) {
-    header("Location: index.php?accion=redireccion");
-} 
+  header("Location: index.php?accion=redireccion");
+}
 
 include_once("./Views/include/UH.php");
 ?>
 
-    <title>Registro</title>
+  <title>Registro</title>
+
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+
+<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+
+<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+
 
 <div class="contenedor-formulario">
-    <section class="formularios99">
-        <h3>Registrarse como Tecnico</h3>
+  <section class="formularios99">
+    <h3>Registrarse como Tecnico</h3>
 
-        <form method="POST" action="index.php?accion=guardarT" enctype="multipart/form-data">
-
+    <form method="POST" action="index.php?accion=guardarT" enctype="multipart/form-data">
+            
             <p class="fade-label">Nombre de Usuario</p>
-            <input type="text" class="form-control" id="usuario" name="usuario" autocomplete="off" placeholder="sin caracteres especiales" required> <br><br>
+      <input type="text" class="form-control" id="usuario" name="usuario" autocomplete="off" placeholder="sin caracteres especiales" required> <br><br>
 
-            <p class="fade-label">Email </p>
-            <label for="mail" class="form-label"></label>
-            <input type="email" pattern="^[\p{L}0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" class="form-control" id="mail" name="mail" autocomplete="off" required> <br><br>
-            
-
-            <div id="tecnico-fields">
-                <p class="fade-label">Especializaciones (Seleccione al menos 1)</p>
-                    
-                <select class="form-control select2" name="especializaciones[]" multiple> 
-                    <?php foreach ($especializaciones as $esp): ?>
-                        <option value="<?php echo htmlspecialchars($esp['id']); ?>">
-                            <?php echo htmlspecialchars($esp['nombre']); ?>
-                        </option>
-                    <?php endforeach; ?>
-                </select>
-                <br>
-                <br>
-            
-
-            
-                <p class="fade-label">Otra Especialidad (Opcional)</p>
-                
-                <input type="text" class="form-control" name="otra_especialidad" placeholder="Ej: Reparación GPU"> 
-                
-                <br>
-                <br>
-            </div>
+      <p class="fade-label">Email </p>
+      <label for="mail" class="form-label"></label>
+      <input type="email" pattern="^[\p{L}0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$" class="form-control" id="mail" name="mail" autocomplete="off" required> <br><br>
+      
+      <div id="tecnico-fields">
+        <p class="fade-label">Especializaciones (Seleccione al menos 1)</p>
+         
+        <select class="form-control select2" name="especializaciones[]" multiple>
+          <?php foreach ($especializaciones as $esp): ?>
+            <option value="<?php echo htmlspecialchars($esp['id']); ?>">
+              <?php echo htmlspecialchars($esp['nombre']); ?>
+            </option>
+          <?php endforeach; ?>
+        </select>
+        <br><br>
+     
+        <p class="fade-label">Otra Especialidad (Opcional)</p>
+        <input type="text" class="form-control" name="otra_especialidad" placeholder="Ej: Reparación GPU">
+        <br><br>
+      </div>
 
             <p class="fade-label">Contraseña</p>
-            <div class="password-container">
-            <input type="password" class="form-control" id="contrasena" name="contrasena" minlength="8" placeholder="mínimo 8 caracteres" required>
-            <i class="fa-solid fa-eye toggle-password" onclick="togglePassword('contrasena', this)"></i>
-            </div>
-            <br>
+      <div class="password-container">
+      <input type="password" class="form-control" id="contrasena" name="contrasena" minlength="8" placeholder="mínimo 8 caracteres" required>
+      <i class="fa-solid fa-eye toggle-password" onclick="togglePassword('contrasena', this)"></i>
+      </div>
+      <br>
 
-            <p class="fade-label">Confirmar Contraseña</p>
-            <div class="password-container">
-            <input type="password" class="form-control" id="confirmar_contrasena" name="confirmar_contrasena" minlength="8" required>
-            <i class="fa-solid fa-eye toggle-password" onclick="togglePassword('confirmar_contrasena', this)"></i>
-            </div>
-            <p id="error-password" class="error-text"></p>
-            <br>
+      <p class="fade-label">Confirmar Contraseña</p>
+      <div class="password-container">
+      <input type="password" class="form-control" id="confirmar_contrasena" name="confirmar_contrasena" minlength="8" required>
+      <i class="fa-solid fa-eye toggle-password" onclick="togglePassword('confirmar_contrasena', this)"></i>
+      </div>
+      <p id="error-password" class="error-text"></p>
+      <br>
 
-            <input class="button" type="submit" value="Guardar">
-            <a href="Index.php?accion=login" class="login9" >¿Ya tiene una cuenta? Inicie sesión</a>
+      <input class="button" type="submit" value="Guardar">
+      <a href="Index.php?accion=login" class="login9" >¿Ya tiene una cuenta? Inicie sesión</a>
 
-        </form>
-    </section>
-    <br>
+    </form>
+  </section>
+  <br>
 </div>
 
 <script>
-    $(document).ready(function() {
-        $('.select2').select2({
-            // Puedes añadir opciones aquí, por ejemplo:
-            placeholder: "Selecciona una o más especializaciones",
-            allowClear: true // Permite deseleccionar todos los elementos
-        });
-    });
+$(document).ready(function() {
+  $('.select2').select2({
+    placeholder: "Selecciona una o más especializaciones",
+    allowClear: true,
+    width: '100%',
+    closeOnSelect: false,
+    language: {
+      noResults: function() {
+        return "No se encontraron resultados";
+      }
+    }
+  });
+});
 </script>
 
-<script>
-    // Se ejecuta una vez que el DOM está completamente cargado
-    $(document).ready(function() {
-        // Inicializa Select2 en el elemento con la clase 'select2'
-        $('.select2').select2({
-            placeholder: "Selecciona una o más especializaciones",
-            allowClear: true
-        });
-    });
-</script>
 
 <script src="Assets/js/imagenformulario.js"></script>
 <script src="Assets/js/vercontrasena.js"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> 
-<link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
-<script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
