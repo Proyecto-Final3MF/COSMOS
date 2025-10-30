d30e0c46cafd23335218243b388842f9ab553be4
AlfreThiago <alfrethiagoc@gmail.com>
2025-10-30 15:45:41 -0300

    Review mas seguro

--- Models/ReviewM.php
+++ Models/ReviewM.php
@@ -164,4 +164,25 @@ class Review {
         $fila = $resultado->fetch_assoc(); 
         return $fila['suma_rating']; 
     }
+
+    public function checkUsuario($id_solicitud, $id_usuario) {
+        $sql = "SELECT id FROM solicitud WHERE id = ? AND cliente_id = ?";
+        $stmt = $this->conn->prepare($sql);
+
+        if ($stmt === false) {
+            return false;
+        }
+
+        $stmt->bind_param("ii", $id_solicitud, $id_usuario);
+        $stmt->execute();
+        $resultado = $stmt->get_result();
+.
+        if ($resultado->num_rows > 0) {
+            $stmt->close();
+            return true;
+        } else {
+            $stmt->close();
+            return false;
+        }
+    }
 }
