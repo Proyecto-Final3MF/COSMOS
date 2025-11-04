938c9c25dcdcfa084d086f0661d6591952688bf5
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-11-04 20:07:09 -0300

    pequeño cambito

--- Controllers/PublicC.php
+++ Controllers/PublicC.php
@@ -1,19 +1,19 @@
 <?php
 class PublicC
 {
-
     public function inicio()
     {
-        include(__DIR__ . "Views/Usuario/inicio.php");
+        include(__DIR__ . "/../Views/Usuario/inicio.php");
     }
 
     public function nosotros()
     {
-        include(__DIR__ . "Views/Usuario/nosotros.php");
+        include(__DIR__ . "/../Views/Usuario/nosotros.php");
     }
 
     public function contacto()
     {
-        include(__DIR__ . "Views/Usuario/contacto.php");
+        include(__DIR__ . "/../Views/Usuario/contacto.php");
     }
 }
+
