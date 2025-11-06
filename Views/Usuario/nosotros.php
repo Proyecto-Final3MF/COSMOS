ab6f05409b19ef8f1d987d72047ced4f3757f2c8
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-11-05 22:12:03 -0300

    cambios

--- Views/Usuario/nosotros.php
+++ Views/Usuario/nosotros.php
@@ -10,9 +10,9 @@
 
 <br>
 
-<h1 class="inicio55 reveal"> Sobre Nosotros </h1>
+<h1 class="inicio55"> Sobre Nosotros </h1>
 
-<p class="inicio44 reveal">
+<p class="inicio44">
     Les presento COSMOS, una plataforma web creada para conectar de forma 
     rápida, segura y eficiente a clientes con técnicos informáticos calificados. 
     En pocas palabras: si se te rompe un dispositivo y necesitás ayuda urgente, 
@@ -21,11 +21,11 @@
 
 <br><br>
 
-<h2 class="nosotros reveal">
+<h2 class="nosotros">
     Cómo Nació
 </h2>
 
-<p class="nosotro reveal">
+<p class="nosotro">
     Todos hemos pasado por ese momento en que nuestra computadora, celular o 
     tablet deja de funcionar justo cuando más la necesitamos, ¿verdad? 
     Y lo peor es que no sabemos a quién acudir, los servicios tardan, o simplemente 
@@ -38,43 +38,54 @@
 
 <br>
 
-<h2 class="nosotros reveal">
+<h2 class="nosotros">
     Desarrolladores
 </h2>
 
-<p class="nosotro reveal">
+<p class="nosotro">
     La aplicacion COSMOS fue desarrolada por:
 </p>
 
 <section class="equipo-container">
-    <div class="miembro reveal">
+    <a href="https://github.com/FMS-php"> 
+    <div class="miembro">
         <img src="Assets/imagenes/perfil/elfede.jpeg" alt="Miembro 1">
         <h3>Federico Mosegui</h3>
     </div>
+    </a>
 
-    <div class="miembro reveal">
+    <a href="https://github.com/AlfreThiago">
+    <div class="miembro">
         <img src="Assets/imagenes/perfil/elthiago.jpeg" alt="Miembro 2">
         <h3>Thiago Carballo</h3>
     </div>
+    </a>
 
-    <div class="miembro reveal">
+    <a href="https://github.com/A-R-G-B">
+    <div class="miembro">
         <img src="Assets/imagenes/perfil/yoese.jpeg" alt="Miembro 3">
-        <h3>Alex Gonzalez</h3>
+        <h3>Alex <br> Gonzalez</h3>
     </div>
-
-    <div class="miembro reveal">
+    </a>
+    
+    <a href="https://github.com/SerDan40">
+    <div class="miembro">
         <img src="Assets/imagenes/perfil/elsergio.jpeg" alt="Miembro 4">
-        <h3>Sergio Lopez</h3>
+        <h3>Sergio <br> Lopez</h3>
     </div>
+    </a>
 
-    <div class="miembro reveal">
+    <a href="https://github.com/VectorAlonne">
+    <div class="miembro">
         <img src="Assets/imagenes/perfil/ellucas.jpeg" alt="Miembro 5">
-        <h3>Lucas Vargas</h3>
+        <h3>Lucas <br> Vargas</h3>
     </div>
+    </a>
 
-    <div class="miembro reveal">
+    <a href="https://github.com/FrancoFagundez">
+    <div class="miembro">
         <img src="Assets/imagenes/perfil/elfranco.jpeg" alt="Miembro 6">
         <h3>Franco Fagundez</h3>
     </div>
+    </a>
 </section>
-<script src="Assets/js/aparicion.js"></script>
