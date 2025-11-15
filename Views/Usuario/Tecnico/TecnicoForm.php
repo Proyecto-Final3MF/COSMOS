2375392a31044127548afacaf1c03c65e1f81ac8
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-11-14 22:03:29 -0300

    Update TecnicoForm.php

--- Views/Usuario/Tecnico/TecnicoForm.php
+++ Views/Usuario/Tecnico/TecnicoForm.php
@@ -7,8 +7,15 @@ if (isset($_SESSION['rol']) && in_array($_SESSION['rol'], [1, 2, 3])) {
 require_once(__DIR__ . "../../../include/UH.php");
 ?>
 
-<title>Registro</title>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+</head>
+<body>
+    
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet"/>
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
@@ -115,3 +122,6 @@ $(document).ready(function() {
 
 <script src="Assets/js/imagenformulario.js"></script>
 <script src="Assets/js/vercontrasena.js"></script>
+
+</body>
+</html>
