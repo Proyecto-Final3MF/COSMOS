b8ac6a0e06d6e2caee235f5d8d69402e2dc373df
Alex Gonzalez <alexgonzalezbenquet@gmail.com>
2025-11-15 21:32:12 -0300

    cambios necesarios

--- Views/Usuario/Cliente/ClienteP.php
+++ Views/Usuario/Cliente/ClienteP.php
@@ -8,8 +8,13 @@ require_once(__DIR__ . "../../../include/UH.php");
 
 ?>
 
-<title>Panel de Cliente</title>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Panel de Cliente</title>
+</head>
 <body>
     <br><br>
 <div>
